680d54b3fce24e3e015c17b667750b469c47f1d1
leezym <elicamodo@hotmail.com>
2025-10-24 10:32:45 -0500

    enlaces funcionales en footer

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -11,17 +11,14 @@ class Footer extends Component {
                 <Segment fixed='bottom' textAlign='center' inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em', backgroundColor: "#28367b" }}>
                     <Image centered size='mini' src={logo} />
                     <List horizontal inverted divided link size='small'>
-                    <List.Item as='a' href='#'>
-                        Site Map
+                    <List.Item as='a' href='https://blog.ubicu.co' target="_blank" rel="noopener noreferrer">
+                        Blog
                     </List.Item>
-                    <List.Item as='a' href='#'>
-                        Contact Us
+                    <List.Item as='a' href='https://blog.ubicu.co/contact' target="_blank" rel="noopener noreferrer">
+                        Contáctenos
                     </List.Item>
-                    <List.Item as='a' href='#'>
-                        Terms and Conditions
-                    </List.Item>
-                    <List.Item as='a' href='#'>
-                        Privacy Policy
+                    <List.Item as='a' href='https://blog.ubicu.co/about' target="_blank" rel="noopener noreferrer">
+                        Quiénes somos
                     </List.Item>
                     </List>
                 </Segment>    
